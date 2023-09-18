6b53c122c763e90946ff5b07788d32068048b99f
Jorge Saldana <jojonoob134@gmail.com>
2023-09-18 13:42:39 -0500

    did the thing

---
+++ PalindromeExercise/PalindromeExercise/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Methods
+    {
+        public string reversed(string word)
+        {
+            string ver = word.Replace(" ", "");
+            char[] rev = ver.ToCharArray();
+            Array.Reverse(rev);
+            string isItPalindrome = new string(rev);
+            return isItPalindrome;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using System.ComponentModel;
+using System.Numerics;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("race car", "racecar")]
+        [InlineData("a man a plan a canal panama", "amanaplanacanalpanama")]
+        [InlineData("level", "level")]
+        [InlineData("madam", "madam")]
+        [InlineData("was it a car or a cat i saw", "wasitacaroracatisaw")]
+        public void Test1(string input, string expected)
         {
+            //throw new NotImplementedException();
+            var isSame = new Methods();
 
+            var sameIs = isSame.reversed(input);
+
+            Assert.Equal(sameIs, expected);
         }
     }
 }
